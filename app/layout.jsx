8f9afa5ec9bdcaace79aa2bd23a94b7a9a8b12c8
fakbo <fakbo0206@gmail.com>
2024-06-20 20:41:02 -0400

    need complete the Header

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
-
+// Components
+import Header from "@/components/Header";
 const poppins = Poppins({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], variable: "--font-poppins" });
 
 export const metadata = {
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.variable}>{children}</body>
+      <body className={poppins.variable}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/Header.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link"
+import Image from "next/image"
+const Header = () => {
+    return (
+        <header className="py-8 xl:py-12">
+            <nav className="container mx-auto flex justify-between items-center">
+                {/* Logo */}
+                <div className="relative">
+                    <Link href="https://pokeapi.co/">
+                        <Image
+                            src="/assets/logo.png"
+                            priority
+                            quality={100}
+                            alt="logo"
+                            className="w-auto h-auto"
+                            width={100}
+                            height={100}
+                        />
+                    </Link>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
